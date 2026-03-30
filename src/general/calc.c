7187de66948c27e50dab6de50c66c30305b65ab8
Extinct <271471047+AmIExtinct@users.noreply.github.com>
2026-03-30 12:36:08 -0500

    Breh

--- src/general/calc.c
+++ src/general/calc.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 void createTable(float *tableOf, int *tableUpto){
+   
    for (int x = 1; x<=*tableUpto; x++) {
-     printf("\t\t %f \t X \t %0.f \t=\t %f \n\n", *tableOf, (float)x, *tableOf*(float)x);
+     printf("\t\t %f  X  %0.f \t=\t %f \n\n", *tableOf, (float)x, *tableOf*(float)x);
    }
 
    printf("-_-\n");
